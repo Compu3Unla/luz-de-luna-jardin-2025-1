145d307170fd69d70304303aee0fe8b4b0ddb272
Lul1zarraga <lucianaalizarragaa@gmail.com>
2025-06-18 23:38:16 -0300

    video

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const boton = document.querySelector('.boton-inicio');
+  const pantalla = document.getElementById('pantalla-inicial');
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+
+});
